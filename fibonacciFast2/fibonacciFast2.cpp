6f70aaf42b2d637747aeb5978a4264477fd9a24b
AH11037 <abdirahmanhussein2006@gmail.com>
2025-12-16 19:55:51 +0000

    Add project files.

---
+++ fibonacciFast2/fibonacciFast2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <chrono>
+#include <iomanip>
+#include <cstdint>
+#include <fstream>
+using namespace std;
+using boost::multiprecision::cpp_int;
+using namespace std::chrono;
+
+void printerThing(const cpp_int& eggs) {
+    ofstream textFile("fibonacci.txt");
+    textFile << eggs;
+    textFile.close();
+}
+
+
+vector<vector<cpp_int>> multiplyMatrixes(const vector<vector<cpp_int>>& x, const vector<vector<cpp_int>>& y) {
+    return {
+        {x[0][0] * y[0][0] + x[0][1] * y[1][0],
+        x[0][0] * y[0][1] + x[0][1] * y[1][1]},
+        {x[1][0] * y[0][0] + x[1][1] * y[1][0],
+        x[1][0] * y[0][1] + x[1][1] * y[1][1]},
+    };
+}
+
+vector<vector<cpp_int>> squareMatrix(vector<vector<cpp_int>> e, long long n) {
+    vector<vector<cpp_int>> result = { {1,0},{0,1} };
+    while (n > 0) {
+        if (n & 1) result = multiplyMatrixes(result, e);
+        e = multiplyMatrixes(e, e);
+        n >>= 1;
+    }
+    return result;
+}
+
+cpp_int fibonacciMainFunction(long long n) {
+    if (n <= 1) return n;
+    vector<vector<cpp_int>> mainMatrix = { {1 , 1},{1 , 0} };
+    return squareMatrix(mainMatrix, n - 1)[0][0];
+}
+
+int main() {
+    long long nThTerm;
+    cout << "What nth term do you want?:";
+    cin >> nThTerm;
+    auto start = high_resolution_clock::now();
+    cpp_int finalResult = fibonacciMainFunction(nThTerm);
+    auto end = high_resolution_clock::now();
+    auto duration_micro = duration_cast<microseconds>(end - start).count();
+    auto duration_sec = duration_cast<duration<double>>(end - start).count();
+    cout << "It took " << duration_micro << " microseconds (" << duration_sec << " seconds)\n";
+    printerThing(finalResult);
+    cout << "Open fibonacci.txt. The number is ready.\n";
+    return 0;
+}
