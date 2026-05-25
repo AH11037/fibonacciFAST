0c0e36f6718a5e3401271bd2fa2b68f683a3da7f
temp name <abdirahman123789@gmail.com>
2026-05-25 15:38:00 +0100

    inital

---
+++ CMakeFiles/Fibonaccibutfast.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fibonaccibutfast.dir/link.d"
+  "CMakeFiles/Fibonaccibutfast.dir/fibonacciFast2.cpp.o"
+  "CMakeFiles/Fibonaccibutfast.dir/fibonacciFast2.cpp.o.d"
+  "Fibonaccibutfast"
+  "Fibonaccibutfast.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fibonaccibutfast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Fibonaccibutfast.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Fibonaccibutfast.
+# This may be replaced when dependencies are built.
